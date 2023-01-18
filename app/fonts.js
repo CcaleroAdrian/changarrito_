20fef28e73b1457ec582f7cbcff351c97e7f7108
@adrianC <calero.adrian93@gmail.com>
2023-01-18 11:31:31 -0600

    Se agregan archivos fuentes

---
+++ app/fonts.js
@@ -0,0 +1,12 @@
+import { Love_Ya_Like_A_Sister, Work_Sans } from '@next/font/google';
+
+export const love_ya_like_a_sister = Love_Ya_Like_A_Sister({
+  subsets: ['latin'],
+  weight: ['400'],
+  display: 'optional'
+});
+
+export const work_sans = Work_Sans({
+  subsets: ['latin'],
+  display: 'optional',
+});
---
+++ app/page.jsx
@@ -0,0 +1,27 @@
+'use client'
+import * as React from 'react';
+import Button from 'react-bootstrap/Button';
+import Stack from 'react-bootstrap/Stack';
+import { love_ya_like_a_sister, work_sans } from './fonts';
+
+export default function Home() {
+  return (
+    <>
+      <Stack direction="horizontal" gap={2}>
+        <Button variant="primary">Primary</Button>{' '}
+        <Button variant="secondary">Secondary</Button>{' '}
+        <Button variant="success">Success</Button>{' '}
+        <Button variant="warning">Warning</Button>{' '}
+        <Button variant="danger">Danger</Button>{' '}
+        <Button variant="info">Info</Button>{' '}
+        <Button variant="light">Light</Button>{' '}
+        <Button variant="dark">Dark</Button>
+        <Button variant="link">Link</Button>
+      </Stack>
+      <h1 className={love_ya_like_a_sister.className}>Titulos</h1>
+      <p className={work_sans.className}>
+          Ipsum sint exercitation fugiat ullamco culpa consectetur laborum excepteur. Aliquip sint id cupidatat ex ullamco proident ipsum sint sit esse cillum ullamco. Dolore ea deserunt mollit adipisicing sunt duis cupidatat do anim consequat veniam.
+      </p>
+    </>
+  )
+}
