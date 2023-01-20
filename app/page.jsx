b1545479439a4c46a1a03966f7cc300e92d179b7
AgustinGodinez <105577729+AgustinGodinez@users.noreply.github.com>
2023-01-20 01:55:55 -0600

    loginPage
    todo el archivo app/Login es para que se vea en pantalla completa

--- app/page.jsx
+++ app/page.jsx
@@ -6,33 +6,14 @@ import { love_ya_like_a_sister, work_sans } from './fonts';
 import ModalFoot from './ModalFoot';
 import Footer from './Footer';
 import CardInfo from './CardInfo';
+import { Login } from './Login';
 
 
 export default function Home() {
   return (
     
     <div>
-      <Stack direction="horizontal" gap={2}>
-        <Button variant="primary">Primary</Button>{' '}
-        <Button variant="secondary">Secondary</Button>{' '}
-        <Button variant="success">Success</Button>{' '}
-        <Button variant="warning">Warning</Button>{' '}
-        <Button variant="danger">Danger</Button>{' '}
-        <Button variant="info">Info</Button>{' '}
-        <Button variant="light">Light</Button>{' '}
-        <Button variant="dark">Dark</Button>
-        <Button variant="link">Link</Button>
-      </Stack>
-      <h1 className={love_ya_like_a_sister.className}>Titulos</h1>
-      <p className={work_sans.className}>
-          Ipsum sint exercitation fugiat ullamco culpa consectetur laborum excepteur. Aliquip sint id cupidatat ex ullamco proident ipsum sint sit esse cillum ullamco. Dolore ea deserunt mollit adipisicing sunt duis cupidatat do anim consequat veniam.
-      </p>
-      {/* componente ModalFoot */}
-      <ModalFoot></ModalFoot>
-      {/* componente de card de mas informacio */}
-      <CardInfo></CardInfo>
-      {/* componente Footer */}
-      <Footer></Footer>
+      <Login></Login>
     </div>
   )
 }
